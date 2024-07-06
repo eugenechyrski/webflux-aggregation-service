9c5c0284d4b5b4278b410abd7cd7736e37c5e064
Eugene Chyrski <eugene-chyrski@idexx.com>
2024-07-06 12:47:25 +0200

    add timeouts and retry logic

--- src/main/java/com/chyrskiconsulting/accessment/fedex/aggregation/service/AggregationService.java
+++ src/main/java/com/chyrskiconsulting/accessment/fedex/aggregation/service/AggregationService.java
@@ -4,13 +4,13 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,7 +18,6 @@ import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.core.scheduler.Schedulers;
-import reactor.netty.http.client.HttpClient;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
@@ -26,8 +25,9 @@ public class AggregationService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AggregationService.class);
     private static final int AGGREGATION_TIMEOUT_SEC = 5;
     private static final int AGGREGATE_REQUESTS = 5;
+    private static final int RETRY_COUNT = 5;
     private static final int BATCH_PROCESSING_PARALLELISM = 500;
-    private static final Duration MAX_RESPONSE_TIMEOUT_MS = Duration.ofSeconds(5);
+    private static final Duration RESPONSE_SLA = Duration.ofSeconds(10);
 
     private final WebClient webClient;
     private final String baseUrl;
@@ -41,8 +41,7 @@ public class AggregationService {
     private final ReentrantLock shipmentsSinkLock = new ReentrantLock();
 
     public AggregationService(WebClient.Builder webClientBuilder, String baseUrl) {
-        HttpClient client = HttpClient.create().responseTimeout(MAX_RESPONSE_TIMEOUT_MS);
-        this.webClient = webClientBuilder.baseUrl(baseUrl).clientConnector(new ReactorClientHttpConnector(client)).build();
+        this.webClient = webClientBuilder.baseUrl(baseUrl).build();
         this.baseUrl = baseUrl;
     }
 
@@ -97,16 +96,19 @@ public class AggregationService {
                 .bufferTimeout(AGGREGATE_REQUESTS, Duration.ofSeconds(AGGREGATION_TIMEOUT_SEC))
                 .filter(batch -> !batch.isEmpty())
                 .parallel(BATCH_PROCESSING_PARALLELISM).runOn(Schedulers.newParallel(uriTemplate))
+
                 .flatMap(batch -> {
                     List<Tuple2<String, ResponseCollector<T>>> requests = List.copyOf(batch);
                     String queries = requests.stream().map(Tuple2::getT1).distinct().collect(Collectors.joining(","));
                     LOGGER.debug("Starting aggregation processing for {}", queries);
                     return webClient.get()
                             .uri(uriTemplate, queries)
-
                             .retrieve()
                             .bodyToMono(responseType)
-                            .onErrorResume(this::processOnError)
+                            .retry(RETRY_COUNT)
+                            .timeout(RESPONSE_SLA)
+                            .onErrorResume(TimeoutException.class, this::processTimeout)
+                            .onErrorResume(this::processThrowable)
                             .doOnNext(response -> requests.forEach(request -> processResponse(request.getT1(), response, request.getT2())));
                 })
                 .subscribe();
@@ -117,7 +119,12 @@ public class AggregationService {
         responseCollector.submit(Tuples.of(request, Optional.ofNullable(response.get(request))));
     }
 
-    private <T> Mono<Map<String, T>> processOnError(Throwable error) {
+    private <T> Mono<Map<String, T>> processTimeout(Throwable error) {
+        LOGGER.warn("Timed out while awaiting from the response from downstream service.");
+        return Mono.just(Map.of());
+    }
+
+    private <T> Mono<Map<String, T>> processThrowable(Throwable error) {
         LOGGER.error(error.getMessage(), error);
         return Mono.just(Map.of());
     }
